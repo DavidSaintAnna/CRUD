7a5d6dd88fe86ba29869589953b83c64a6315fef
David Henri <david.henri.santana@gmail.com>
2024-12-13 07:18:33 -0300

    fix(semantic): kebab case and useless classes

---
+++ frontend/src/main.js
@@ -0,0 +1,6 @@
+const inputCreate = document.getElementById("create");
+const btnId = document.getElementById("create-list");
+const searchInput = document.getElementById("filter-input");
+const filterDropdown = document.getElementById("filter-dropdown");
+const listOfTodo = document.getElementById("content");
+
